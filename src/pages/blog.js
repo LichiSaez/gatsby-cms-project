42ae2ccb8ee90581ecd643e56fd5bf34d4b30497
Lisandro Saez <lisandrosaezamelio@gmail.com>
2021-02-25 12:22:11 -0300

    pre exercise commit

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BlogPage = () => {
+    return (
+        <div>
+            <h1>Blog</h1>
+            <p>Posts here</p>
+        </div>
+    );
+};
+
+export default BlogPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react";
 
-export default () => <div>The Great Gatsby</div>
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <h2>I'm a React Developer</h2>
+        </div>
+    )
+};
+
+export default IndexPage;
