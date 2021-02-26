9359858d311cdd07007839c229c4c2b6608a8e8b
Lisandro Saez <lisandrosaezamelio@gmail.com>
2021-02-26 20:49:27 -0300

    adding scss modules for styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,38 @@
 import React from "react";
-import {Link} from 'gatsby';
+import { Link } from "gatsby";
+import headerStyles from "./header.module.scss";
 
 const Header = () => {
   return (
-    <header>
-      <h1>Lisandro Saez</h1>
+    <header className={headerStyles.header}>
+      <h1>
+        <Link to="/" className={headerStyles.title}>
+          Lisandro Saez
+        </Link>
+      </h1>
       <nav>
-          <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/about">About</Link></li>
-              <li><Link to="/blog">Blog</Link></li>
-              <li><Link to="/contact">Contact Me</Link></li>
-          </ul>
+        <ul className={headerStyles.navList}>
+          <li>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">
+              Contact Me
+            </Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
