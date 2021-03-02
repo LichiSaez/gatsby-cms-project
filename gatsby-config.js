c926d470bc4b326df74518694fdee1f78cbfcd4a
Lisandro Saez <lisandrosaezamelio@gmail.com>
2021-03-02 17:37:58 -0300

    sourcing the md posts to the blog page

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,14 @@ module.exports = {
     author: "Lisandro Saez"
   },
   plugins: [
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    },
+    'gatsby-transformer-remark'
   ],
 };
