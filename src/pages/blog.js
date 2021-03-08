ff3d49b24e7528378aaece03170d5b281fb11fdb
Lisandro Saez <lisandrosaezamelio@gmail.com>
2021-03-08 19:46:59 -0300

    linking the posts from blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,39 +1,44 @@
-import React from 'react';
+import React from "react";
 import Layout from "../components/layout";
-import {graphql, useStaticQuery} from 'gatsby';
+import { Link, graphql, useStaticQuery } from "gatsby";
 
 const BlogPage = () => {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query {
-        allMarkdownRemark {
-            edges {
-              node {
-                frontmatter {
-                  title
-                  date
-                    }
-                }
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
             }
+            fields {
+              slug
+            }
+          }
         }
-    }           
+      }
+    }
   `);
-    return (
-        <div>
-            <Layout>
-            <h1>Blog</h1>
-            <ol>
-                {data.allMarkdownRemark.edges.map((edge) => {
-                    return (
-                        <li>
-                            <h2>{edge.node.frontmatter.title}</h2>
-                            <p>{edge.node.frontmatter.date}</p>
-                        </li>
-                    );
-                })}
-            </ol>
-            </Layout>
-        </div>
-    );
+  return (
+    <div>
+      <Layout>
+        <h1>Blog</h1>
+        <ol>
+          {data.allMarkdownRemark.edges.map((edge) => {
+            return (
+              <li>
+                <Link to={`/blog/${edge.node.fields.slug}`}>
+                    <h2>{edge.node.frontmatter.title}</h2>
+                    <p>{edge.node.frontmatter.date}</p>
+                </Link>
+              </li>
+            );
+          })}
+        </ol>
+      </Layout>
+    </div>
+  );
 };
 
 export default BlogPage;
